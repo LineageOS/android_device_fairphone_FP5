cde5a73121f8be778a3c4fbdb02dc1db9756ba61
Rohit Sekhar <git@merothh.com>
2023-08-24 13:04:00 +0530

    FP5: sepolicy: Initial rules
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Tommy Webb <tommy@calyxinstitute.org>
    Change-Id: Ie5544499e7341a817f3b0352693abf0093b6e80a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,6 @@ VENDOR_SECURITY_PATCH := 2024-03-05
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
