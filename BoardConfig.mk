502781b2691f31a766a82c07516a27bbe8dca9bc
Michael Bestas <mkbestas@lineageos.org>
2023-08-30 14:18:49 +0300

    FP5: Update manifests from TT4C
    Change-Id: I03cb478ca5ac82c75bdfa3683dc00927722c3328

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest_yupik.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Kernel
