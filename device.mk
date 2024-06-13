f03c7696e7746fa98429bf0a910cff9a44ea7f79
Chirayu Desai <chirayudesai1@gmail.com>
2024-06-13 22:02:20 +0530

    FP5: Disable UFFD GC
    Unable to determine UFFD GC flag for kernel version "5.4.268-qgki".
    You can fix this by explicitly setting PRODUCT_ENABLE_UFFD_GC to "true" or
    "false" based on the kernel version.
    1. Set PRODUCT_ENABLE_UFFD_GC to "true" if the kernel supports userfaultfd(2)
       and MREMAP_DONTUNMAP.
    2. Set PRODUCT_ENABLE_UFFD_GC to "false" otherwise.
    
    Change-Id: Ie77d9a86a7fe94e4cade1868ffea27194f724e03

--- device.mk
+++ device.mk
@@ -237,6 +237,9 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml \
