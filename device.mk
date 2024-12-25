caee10ca3b3c1354dbb12b449c5e7e731b5f162a
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    FP5: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -247,6 +247,7 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResCommon \
     FrameworksResTarget \
+    NcmTetheringOverlay \
     SettingsProviderResCommon \
     SettingsResCommon \
     SystemUIResCommon \
