05a6cc264356f2dcaca6ad408bccd4afa9b6859d
Michael Bestas <mkbestas@lineageos.org>
2025-02-04 13:33:37 +0200

    FP5: Bump to FP5.UT2K.B.098.20250109
    Change-Id: I24564d67a209f0f7de5687a0a6a3033088bd0f83

--- lineage_FP5.mk
+++ lineage_FP5.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := Fairphone
 PRODUCT_GMS_CLIENTID_BASE := android-alcatel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Fairphone/FP5/FP5:14/UKQ1.230924.001/UT2H:user/release-keys \
+    BuildFingerprint=Fairphone/FP5/FP5:14/UKQ1.230924.001/UT2K:user/release-keys \
     DeviceProduct=FP5
