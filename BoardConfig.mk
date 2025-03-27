75b94401bcd37ee5ad0ea454e9cecaacaa1c5dec
Michael Bestas <mkbestas@lineageos.org>
2025-03-27 14:44:34 +0200

    FP5: Bump to FP5.UT2L.B.107.20250218
    Change-Id: Ie4a95ea3c0fbef45ce42e266c4a4e87b83064c93

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-BOOT_SECURITY_PATCH := 2025-01-05
+BOOT_SECURITY_PATCH := 2025-02-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
@@ -165,7 +165,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
-STOCK_SECURITY_PATCH := 2025-01-05 # Latest stock update available
+STOCK_SECURITY_PATCH := 2025-02-05 # Latest stock update available
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
