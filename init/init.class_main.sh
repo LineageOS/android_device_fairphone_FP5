6d3f6d186d9459a6e0fd53a177833f8c341e08bf
Michael Bestas <mkbestas@lineageos.org>
2025-07-25 22:15:44 +0300

    FP5: Update to FP5.VT28.C.042.20250623
    Change-Id: I0b2ed15bc136bc850bc7a389fe75edc189d64f15

--- init/init.class_main.sh
+++ init/init.class_main.sh
@@ -132,7 +132,8 @@ case "$baseband" in
             fi
             ;;
         "concurrent")
-            start vendor.dataqti
+            #[FEATURE]-Modify by shaopan.tang 2024-11-11 FP5V-216 Disable RMNET Reference qcom doc: 80-PF777-127
+            #start vendor.dataqti
             if [ "$low_ram" != "true" ]; then
               start vendor.dataadpl
             fi
