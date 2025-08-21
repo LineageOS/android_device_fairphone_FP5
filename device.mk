eb044dbb449a821d690ef29c69a14e69bc9cb151
LuK1337 <priv.luk@gmail.com>
2025-08-21 19:01:20 +0200

    FP5: Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d
    (cherry picked from commit 6bed0e3c5f87a7793718d35ca585e0fe679c6b0d)

--- device.mk
+++ device.mk
@@ -92,16 +92,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
-
-$(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
