49be25f16e23594489a54c6f094de9218b7390e9
Michael Bestas <mkbestas@lineageos.org>
2026-02-28 06:05:38 +0200

    FP5: Update to FP5.VT2M.C.084.20260128
    Change-Id: I0b8a4f6d16877e332c43791aca0b4d78ca76b948

--- lineage_FP5.mk
+++ lineage_FP5.mk
@@ -28,5 +28,5 @@ PRODUCT_MANUFACTURER := Fairphone
 PRODUCT_GMS_CLIENTID_BASE := android-alcatel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Fairphone/FP5/FP5:15/AQ3A.240912.001/VT2K:user/release-keys \
+    BuildFingerprint=Fairphone/FP5/FP5:15/AQ3A.240912.001/VT2M:user/release-keys \
     DeviceProduct=FP5
