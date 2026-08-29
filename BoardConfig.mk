2b1d011380b6941881a565464500451a394e5700
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    FP5: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,7 +91,6 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := vendor/fp5-qgki_defconfig vendor/fp5.config
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/fairphone/qcm6490
 
 # Kernel modules
