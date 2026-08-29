1f340fae1f3b08293988f46e9b8a1b8b3725fb5a
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    FP5: Drop UFFD flag
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- device.mk
+++ device.mk
@@ -203,9 +203,6 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
